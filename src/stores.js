db891c7df8201d22994834c4353cb530010323b0
trevor <me@trevorbehnke.com>
2021-10-04 00:45:56 -0700

    chores

--- src/stores.js
+++ src/stores.js
@@ -1,12 +1,32 @@
-// stores.js
+// localStore.js
 import { writable } from "svelte/store";
-import { localStore } from "./localStore.js";
 
-export const alert = writable("Welcome to the To-Do list app!");
+export const localStore = (key, initial) => {
+  const toString = (value) => JSON.stringify(value, null, 2);
+
+  if (localStorage.getItem(key) === null) {
+    localStorage.setItem(key, toString(initial));
+  }
+
+  const { subscribe, set, update } = writable(
+    JSON.parse(localStorage.getItem(key))
+  );
+
+  return {
+    subscribe,
+    set: (value) => {
+      localStorage.setItem(key, toString(value));
+      return set(value);
+    },
+    update,
+  };
+};
 
 const initial = [
   { id: 1, name: "Eat", completed: false },
   { id: 2, name: "Sleep", completed: false },
+  { id: 3, name: "Code", completed: false },
+  { id: 4, name: "Repeat", completed: false },
 ];
 
 export const todos = localStore("data", initial);
