a5c789ecb51c3f8019f9ce0b0d8094eded44e66a
trevor <me@trevorbehnke.com>
2021-10-08 17:06:03 -0700

    feat: :sparkles: created graphic component and layered divs

---
+++ src/graphic.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Chart from "./chart.svelte"
+  import Clock from "./clock.svelte"
+  import { todos } from './stores.js'
+</script>
+
+<div class=container>
+  <div class="inner">
+    <Chart todos={$todos}/>
+  </div>
+  <div class="inner">
+    <Clock/>
+  </div>
+</div>
+
+<style>
+  .container {
+    /* width: 300px; */
+    /* height: 300px; */
+    /* margin: 0 auto; */
+    /* background-color: yellow; */
+    /* important part */
+    display: grid;
+    place-items: center;
+    grid-template-areas: "inner-div";
+  }
+
+  .inner {
+    /* height: 100px; */
+    /* width: 100px; */
+    /* important part */
+    grid-area: inner-div;
+  }
+</style>
