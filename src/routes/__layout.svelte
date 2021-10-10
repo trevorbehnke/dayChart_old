9c78492a204f4536956d9e67f6fd4e8db412729d
trevor <me@trevorbehnke.com>
2021-10-09 22:20:42 -0700

    feat: :sparkles: configured footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,13 @@
-<script>import "../app.css";</script>
-<slot></slot>
+<script>
+	import '../app.css';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<div class="mx-auto flex flex-col flex-grow w-full max-w-7xl">
+		<main class="flex flex-col w-full flex-grow py-4 px-4">
+			<slot />
+		</main>
+		<Footer />
+	</div>
+</div>
